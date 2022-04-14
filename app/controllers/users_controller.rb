34482d4e2a5ff6b1b33c2cb3246d73c68f36b1b1
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-04-14 13:44:07 -0700

    commit tree 91438de32fb3ed46f953fada3b8d654ed4957a4a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find_by(user_params)
+        render json: user, include: :items
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :city)
+    end
 end
